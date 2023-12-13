407801de97bb1b40175dcf85e15429f8f1fb5473
Angelus123 <izerefaifelix@gmail.com>
2023-12-14 01:05:44 +0200

    add order functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import usersRoutes from './users/usersRoutes.js';
 import clientsRoutes from './clients/clientsRoutes.js';
+import ordersRoutes from './orders/ordersRoutes.js';
 
 const routes = express.Router();
 
 routes.use('/users', usersRoutes);
 routes.use('/clients', clientsRoutes);
+routes.use('/orders', ordersRoutes);
 
 export default routes;
